5a35a13d549e0e5f317a3f02e2094803b723355e
scottwestseattle <sbwilkinson@yahoo.com>
2020-09-23 18:21:01 -0700

    Added FrontPage mvc

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\FrontPageController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [FrontPageController::class, 'index']);
+
